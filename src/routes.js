0c480b4b26a4e1906b4f9af99bad8b6e4346ca91
rai.maciel <rai.maciel@basis.com.br>
2020-06-30 20:56:43 -0300

    Restrições de login e autenticação funcionais

--- src/routes.js
+++ src/routes.js
@@ -4,13 +4,18 @@ const upload = require('multer')(multerConfig)
 
 const routes = express.Router()
 
+const authMiddleware = require('./app/middlewares/auth')
+const guestMiddleware = require('./app/middlewares/guest')
+
 const UserController = require('./app/controllers/UserController')
 const SessionController = require('./app/controllers/SessionController')
 
-routes.get('/', SessionController.create)
+routes.get('/', guestMiddleware, SessionController.create)
 routes.post('/signin', SessionController.store)
 
-routes.get('/signup', UserController.create)
+routes.use('/app', authMiddleware)
+
+routes.get('/signup', guestMiddleware, UserController.create)
 routes.post('/signup', upload.single('avatar'), UserController.store)
 routes.get('/app/dashboard', (req, res) => res.render('dashboard'))
 
